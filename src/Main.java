671fe52d68d97fbe5d74f66eea2bb43e595ba3f2
pravin-amate <amatepm102@gmail.com>
2024-06-12 13:01:49 +0530

    2 commit

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,7 @@ import java.util.stream.Collectors;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-
+       // This is stat rt of programing
      // join() method
         List<Integer> numbers = Arrays.asList(1, 3, 4, 5, 6, 6, 7, 2);
         numbers.stream().sorted(Comparator.reverseOrder()).skip(1).findFirst().get();
